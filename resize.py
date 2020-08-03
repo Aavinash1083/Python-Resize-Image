163e65dcab2bc9d3cb314b371ad31ffc413fccef
Aavinash1083 <aavinashvijay557@gmail.com>
2020-08-03 08:21:24 +0530

    Newly updated

--- resize.py
+++ resize.py
@@ -1,8 +1,8 @@
 from PIL import Image
-foo = Image.open("/home/aavinash1083/Python/Vijay.jpeg")
-foo.size
+img = Image.open("/home/aavinash1083/CELERY/Vijay.jpeg")
+img.size
 (200,374)
-foo = foo.resize((160,300),Image.ANTIALIAS)
-foo.save("/home/aavinash1083/Python/Vijay1.jpeg",quality=95)
-foo.save("/home/aavinash1083/Python/Vijay2.jpeg",optimize=True,quality=95)
+img = img.resize((160,300),Image.ANTIALIAS)
+img.save("/home/aavinash1083/CELERY/Vijay1.jpeg",quality=95)
+img.save("/home/aavinash1083/CELERY/Vijay2.jpeg",optimize=True,quality=95)
 
